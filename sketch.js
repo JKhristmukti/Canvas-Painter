c616b49a2401cbfd317ca082b422aa6e45aa29af
Jason.A.Khristmukti <64148391+JKhristmukti@users.noreply.github.com>
2020-06-10 20:08:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var database,position;
+var dot;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    dot = createSprite(2000,2000,10,10);
+    dot.shapeColor = "black";
+    var dotPosition = database.ref('dot/position');
+    dotPosition.on("value", readPosition, showError);
+}
+
+function draw(){
+    background("white");
+    
+    drawSprites();
+}
+
+function mouseDragged() {
+    var dot = createSprite(mouseX,mouseY,10,10);
+    dot.shapeColor = "black";
+    writePosition(mouseX,mouseY);
+}
+
+function mouseReleased() {
+    var dot = createSprite(mouseX,mouseY,10,10);
+    dot.shapeColor = "black";
+    writePosition(mouseX,mouseY);
+}
+
+function writePosition(x,y){
+    database.ref('dot/position').set({
+        'x': position.x + x,
+        'y': position.y + y
+    })
+}
+
+function readPosition(data){
+    position = data.val();
+    dot.x = position.x;
+    dot.y = position.y;
+}
+
+function showError() {
+    console.log("Error in code");
+}
